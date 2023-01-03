17d8d7f4c5ef195168a36b4a3b54256dfa21e5d8
paintingofblue <90877067+paintingofblue@users.noreply.github.com>
2023-01-03 21:02:45 +1300

    Change from pastebin to github, and adding some debug stuff

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ log "Fetching arch mirrorlist"
 sudo curl -s "https://archlinux.org/mirrorlist/?country=NZ&protocol=https&ip_version=4&use_mirror_status=on" | sed -e "s/^#Server/Server/" -e "/^#/d" | sudo tee /etc/pacman.d/mirrorlist-arch
 
 log "Fetching pacman config"
-sudo curl -s "https://pastebin.com/raw/1JrtGtKn" -o /etc/pacman.conf
+sudo curl -s "https://raw.githubusercontent.com/paintingofblue/Artix/main/pacman/pacman.conf" -o /etc/pacman.conf
 
 log "Installing Chaotic"
 sudo pacman -Syyu
@@ -25,22 +25,22 @@ log "Updating pacman after installing Chaotic"
 sudo pacman -Syyu
 
 log "Installing dependencies"
-sudo pacman -S base-devel git wget appmenu-gtk-module 
+sudo pacman -S base-devel git wget artix-archlinux-support
 
 log "Installing yay"
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
+git clone https://aur.archlinux.org/yay.git && cd yay && echo -e "y\ny\n" | makepkg -si && cd .. && rm -rf yay
 
 log "Installing programs"
-yay -S kitty flameshot discord spotify telegram-desktop-bin visual-studio-code-bin github-desktop-bin brave-bin minecraft-launcher spicetify-cli ffmpeg4.4 zenity vlc obs-studio-git
+yay -S kitty flameshot discord spotify telegram-desktop-bin visual-studio-code-bin github-desktop-bin brave-bin minecraft-launcher spicetify-cli ffmpeg4.4 zenity vlc obs-studio-git appmenu-gtk-module appmenu-gtk2-module appmenu-gtk3-module appmenu-qt5 gtk-engines bleachbit-git cleanerml-git fabric-installer spicetify-cli 
 
-#log "Install Tela Circle icons"
-#git clone "https://github.com/vinceliuice/Tela-circle-icon-theme" && cd Tela-circle-icon-theme && chmod +x install.sh && ./install.sh -a && cd .. && rm -rf Tela-circle-icon-theme
-
-log "Installing catppuccin"
+log "Installing catppuccin themes"
 yay -S catppuccin-kde-theme-git catppuccin-gtk-theme-mocha catppuccin-wallpapers-git sddm-theme-catppuccin-git
+
+log "Installing catppuccin cursors"
 git clone https://github.com/catppuccin/cursors.git
 cd cursors && cd cursors
 echo *
+sleep 2
 sudo cp -r * /usr/share/icons
 
 log "Installing lightly application style"
@@ -48,3 +48,11 @@ sudo pacman -S lightly-qt
 
 log "Installing fonts"
 yay -S nerd-fonts-complete ttf-apple-emoji
+
+log "Installing adblock"
+yay -S blokator
+sudo blokator --sync
+sudo blokator --apply
+
+log "Installing grub config"
+sudo curl -s "https://raw.githubusercontent.com/paintingofblue/Artix/main/grub/grub" -o /etc/default/grub
