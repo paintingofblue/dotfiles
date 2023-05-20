33706745b4e7ba6ba22e73ef845804da66035b82
paintingofblue <90877067+paintingofblue@users.noreply.github.com>
2023-05-20 15:48:27 +1200

    Move configuration files into cfg folder

--- install.sh
+++ install.sh
@@ -25,15 +25,19 @@ sudo sed -i 's/GRUB_TIMEOUT="3"/GRUB_TIMEOUT="0"/g' /etc/default/grub
 sudo sed -i 's/GRUB_GFXMODE="1024x768,800x600"/GRUB_GFXMODE=1920x1080/g' /etc/default/grub
 sudo update-grub
 
-log "Initiating pacman key"
+
+log "Initiating and populating pacman key"
 sudo pacman-key --init
 sudo pacman-key --populate artix
+
 
 log "Fetching arch mirrorlist"
 sudo curl -s "https://archlinux.org/mirrorlist/?country=NZ&protocol=https&ip_version=4&use_mirror_status=on" | sed -e "s/^#Server/Server/" -e "/^#/d" | sudo tee /etc/pacman.d/mirrorlist-arch
 
+
 log "Fetching pacman config"
-sudo curl -s "https://raw.githubusercontent.com/paintingofblue/Artix/main/pacman/pacman.conf" -o /etc/pacman.conf
+sudo curl -s "https://raw.githubusercontent.com/paintingofblue/dotfiles/main/cfg/pacman/pacman.conf" -o /etc/pacman.conf
+
 
 log "Installing Chaotic AUR"
 sudo pacman -Syyu
@@ -43,12 +47,15 @@ sudo pacman-key --populate artix # Populating artix again because it fucks itsel
 sudo pacman -U "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst" "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
+
 log "Updating pacman after installing Chaotic"
 sudo pacman -Syyu
+
 
 log "Installing dependencies"
 sudo pacman -S base-devel git wget artix-archlinux-support
 sudo pacman-key --populate archlinux
+
 
 log "Installing yay"
 git clone https://aur.archlinux.org/yay.git
@@ -56,32 +63,44 @@ cd yay || exit
 makepkg -si
 rm -rf yay
 
+
 log "Installing programs"
 yay -S kitty flameshot discord spotify telegram-desktop-bin visual-studio-code-bin github-desktop-bin brave-bin minecraft-launcher vlc obs-studio-git bleachbit-git cleanerml-git fabric-installer spicetify-cli steam neovim
+
+
+log "Uninstalling unneeded programs"
+sudo pacman -R falkon konqueror plasma-workspace-wallpapers gwenview okular mpv
+
 
 log "Install programming languages"
 yay -S rustup
 rustup default stable
 
+
 log "Installing dependencies"
 yay -S appmenu-gtk-module gtk-engines
+
 
 log "Installing lightly application style"
 sudo pacman -S lightly-qt
 
+
 log "Installing fonts"
 yay -S ttf-apple-emoji
+
 
 log "Installing adblock"
 yay -S blokator
 sudo blokator --sync
 sudo blokator --apply
 
-log "Installing things needed for dotfiles"
-yay -S betterlockscreen brightnessctl bspwm cava chezmoi dunst eww feh file-roller gvfs inter-font jq kripton-theme-git lxappearance maim moreutils pamixer picom picom-animations-git playerctl polybar recode rofi sxhkd thunar ttf-firacode-nerd ttf-ubuntu-mono-nerd xdg-user-dirs xdo xorg xqp
 
-log "Uninstalling unwanted programs"
-sudo pacman -R falkon konqueror plasma-workspace-wallpapers gwenview okular mpv
+log "Installing things needed for my rice"
+yay -S betterlockscreen brightnessctl bspwm cava chezmoi dunst eww feh file-roller gvfs inter-font jq kripton-theme-git lxappearance maim moreutils pamixer picom-animations-git playerctl polybar-git recode rofi sxhkd thunar ttf-firacode-nerd ttf-ubuntu-mono-nerd xdg-user-dirs xdo xorg xqp
+
+
+log "Installing the actual configuration files"
+
 
 log "Clear cache"
 sudo rm -rf ~/.cache/*
